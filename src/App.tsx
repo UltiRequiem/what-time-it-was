5ef93d83cc221c3672baf10860c7b268cf885e05
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-10 10:39:08 -0500

    :recycle: Update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export function App() {
   };
 
   return (
-    <main>
+    <main className="bg-slate-600 flex flex-col p-5 h-screen gap-4 text-2xl text-center">
       <h1>How many Hours ago?</h1>
       <select onInput={hoursOnInput}>
         {Array.from({ length: 25 }, (_value, index) => {
@@ -27,6 +27,8 @@ export function App() {
       </select>
 
       <p>{input}</p>
+
+      <footer className="text-cyan-500">Eliaz Bobadilla</footer>
     </main>
   );
 }
